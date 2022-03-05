5add94f74e38356325320841e3589456f1106b52
Iselin Thorsen Nilsen <iselin.thorsen.nilsen@gmail.com>
2022-03-05 23:30:12 +0100

    Started implementing database activity and corresponding xml

--- app/src/main/java/com/ebookfrenzy/cahiss/activities/DatabaseActivity.java
+++ app/src/main/java/com/ebookfrenzy/cahiss/activities/DatabaseActivity.java
@@ -1,4 +1,20 @@
 package com.ebookfrenzy.cahiss.activities;
 
-public class DatabaseActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import com.ebookfrenzy.cahiss.R;
+import com.ebookfrenzy.cahiss.utils.PersonDAO;
+
+public class DatabaseActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    PersonDAO personDAO;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database);
+    }
 }
