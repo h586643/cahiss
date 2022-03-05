d2807eb68718d3e66d0706af4cec05dbd0e02d21
Iselin Thorsen Nilsen <iselin.thorsen.nilsen@gmail.com>
2022-03-05 23:10:04 +0100

    Implementation of main activity and corresponding xml

--- app/src/main/java/com/ebookfrenzy/cahiss/activities/MainActivity.java
+++ app/src/main/java/com/ebookfrenzy/cahiss/activities/MainActivity.java
@@ -3,14 +3,24 @@ package com.ebookfrenzy.cahiss.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.ebookfrenzy.cahiss.R;
+import com.ebookfrenzy.cahiss.utils.PersonDAO;
 
 public class MainActivity extends AppCompatActivity {
+
+    PersonDAO personDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addPersonButton = findViewById(R.id.addPersonButton);
+        Button startQuizButton = findViewById(R.id.startQuizButton);
+        Button listOfPersonsButton = findViewById(R.id.databaseButton);
+
+        // addPersonButton.setOnClickListener(
     }
 }
