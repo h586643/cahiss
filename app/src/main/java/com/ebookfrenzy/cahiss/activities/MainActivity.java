027066427ff778197785b8f6b74f71c59137e680
Iselin Thorsen Nilsen <iselin.thorsen.nilsen@gmail.com>
2022-03-05 14:31:12 +0100

    Refactor

--- app/src/main/java/com/ebookfrenzy/cahiss/activities/MainActivity.java
+++ app/src/main/java/com/ebookfrenzy/cahiss/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.ebookfrenzy.cahiss;
+package com.ebookfrenzy.cahiss.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.ebookfrenzy.cahiss.R;
 
 public class MainActivity extends AppCompatActivity {
 
