31e50b603af1f8941ee3f1ff51d3bdf667b9978d
Iselin Thorsen Nilsen <iselin.thorsen.nilsen@gmail.com>
2022-03-06 18:12:09 +0100

    Removed import

--- app/src/main/java/com/ebookfrenzy/cahiss/utils/PersonDatabase.java
+++ app/src/main/java/com/ebookfrenzy/cahiss/utils/PersonDatabase.java
@@ -4,7 +4,6 @@ import android.app.Person;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
 
 import androidx.room.Database;
 import androidx.room.Room;
