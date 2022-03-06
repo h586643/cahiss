39ea8a653bae629ae631c4400761c013588d7099
Iselin Thorsen Nilsen <iselin.thorsen.nilsen@gmail.com>
2022-03-06 16:48:20 +0100

    Implemented DatabaseActivity

--- app/src/main/java/com/ebookfrenzy/cahiss/activities/DatabaseActivity.java
+++ app/src/main/java/com/ebookfrenzy/cahiss/activities/DatabaseActivity.java
@@ -1,12 +1,19 @@
 package com.ebookfrenzy.cahiss.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.ebookfrenzy.cahiss.R;
+import com.ebookfrenzy.cahiss.utils.Person;
 import com.ebookfrenzy.cahiss.utils.PersonDAO;
 import com.ebookfrenzy.cahiss.utils.PersonDatabase;
+import com.ebookfrenzy.cahiss.utils.SorterHelper;
+
+import java.util.List;
 
 public class DatabaseActivity extends AppCompatActivity {
 
@@ -21,6 +28,36 @@ public class DatabaseActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.userRecyclerView);
 
         personDAO = PersonDatabase.getDBInstance(this).personDAO();
+        PersonRecycler personRecycler = new PersonRecycler(personDAO.getAllPersons(), this);
 
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(personRecycler);
+    }
+
+    public void sortAlphabetically(View view){
+
+        personDAO = PersonDatabase.getDBInstance(this).personDAO();
+        List<Person> personList = personDAO.getAllPersons();
+
+        SorterHelper sorterHelper = new SorterHelper(personList);
+        sorterHelper.sortAlphabetically();
+        PersonRecycler personRecycler = new PersonRecycler(sorterHelper.getSortedPersonList(), this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(personRecycler);
+    }
+
+    public void sortAlphabeticallyReversed(View view) {
+        personDAO = PersonDatabase.getDBInstance(this).personDAO();
+        List<Person> personList = personDAO.getAllPersons();
+
+        SorterHelper sorterHelper = new SorterHelper(personList);
+        sorterHelper.sortAlphabeticallyReversed();
+        PersonRecycler personRecycler = new PersonRecycler(sorterHelper.getSortedPersonList(), this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(personRecycler);
+    }
+
+    public void addPerson(View v){
+        startActivity(new Intent(DatabaseActivity.this, AddPersonActivity.class));
     }
 }
